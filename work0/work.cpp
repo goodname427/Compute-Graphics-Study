1744ad252c73983292e88762d19203ddc5e439a3
goodname427 <1335438725@qq.com>
2023-10-19 20:56:16 +0800

    saving

--- work0/work.cpp
+++ work0/work.cpp
@@ -9,19 +9,25 @@ int main()
     // 给定一个点 P =(2,1), 将该点绕原点先逆时针旋转 45 ◦ ,再平移 (1,2), 计算出变换后点的坐标(要求用齐次坐标进行计算)。
     
     // init
-    Vector2f p(2, 1);
+    Vector3f p(2, 1, 1);
     std::cout << "original point p:" << std::endl;
     std::cout << p << std::endl;
     
     // rotate
-    Matrix2f rotate_mat;
-    rotate_mat << cos(M_PI_2), -sin(M_PI_2), sin(M_PI_2), cos(M_PI_2);
+    Matrix3f rotate_mat;
+    rotate_mat << cos(M_PI_2), -sin(M_PI_2), 0,
+                sin(M_PI_2), cos(M_PI_2), 0,
+                0, 0, 1;
     p = rotate_mat * p;
     std::cout << "rotated point p:" << std::endl;
     std::cout << p << std::endl;
 
     // translate
-    p += Vector2f(1, 2);
+    Matrix3f translate_mat;
+    translate_mat << 1, 0, 1,
+                    0, 1, 2,
+                    0, 0, 1;
+    p = translate_mat * p;
     std::cout << "translated point p:" << std::endl;
     std::cout << p << std::endl;
 }
