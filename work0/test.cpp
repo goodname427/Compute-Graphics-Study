451c9969d08f063fe5b5ab1d9f29ad4f82beafb9
goodname427 <1335438725@qq.com>
2023-10-19 20:08:14 +0800

    saving

--- work0/test.cpp
+++ work0/test.cpp
@@ -12,7 +12,24 @@ int main()
     n << 1, 1, 
         1, 1;
     
+    // matrix multiply
     std::cout << "a * b =" << std::endl;
     std::cout << (m * n) << std::endl;
-    
+
+    // scale
+    Vector2f v(1, 2);
+    Matrix2f scale_mat;
+    scale_mat(0, 0) = 2;
+    scale_mat(1, 1) = 3;
+    std::cout << "scale_mat * v = " << std::endl 
+        << scale_mat * v << std::endl;
+
+    int a = 2;
+    // x' = x + y / a;
+    // y' = y;
+    Matrix2f shear_mat;
+    shear_mat << 1, a,
+                0, 1;
+    std::cout << "shear_mat * v = " << std::endl 
+        << shear_mat * v << std::endl;
 }
